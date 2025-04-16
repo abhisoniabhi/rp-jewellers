b1a8f474f2cf2150c5d315274fc9dd019bfc2ff0
Mohit Kumar <mohithacky890@gmail.com>
2025-04-16 14:39:25 +0530

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,30 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.rpjewellers.app',
-  appName: 'RP Jewellers',
-  webDir: 'dist/public',
-  server: {
-    androidScheme: 'https',
-    hostname: 'rp-jewellers.onrender.com',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchAutoHide: false,
-      backgroundColor: "#FDE68A",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      spinnerColor: "#B45309",
-      splashFullScreen: true,
-      splashImmersive: true
-    },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"],
-    }
-  }
+  appId: 'com.rpjewellers.app',
+  appName: 'Rp jewellers',
+  webDir: 'dist/public'
 };
 
 export default config;
